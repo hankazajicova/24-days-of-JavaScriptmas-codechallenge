4e1a6bbe3917ee80c1c8d667013d814f0be35be2
zajiha <hanka.zajicova@gmail.com>
2020-12-07 16:57:18 +0100

    day 7

---
+++ src/advent_7_count_vowel_consonant.js
@@ -0,0 +1,36 @@
+// version 1
+function countVowelConsonant(str) {
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
+    const splitted = str.split('');
+    const addCount = splitted.map((item) => (vowels.includes(item)) ? 1 : 2 );
+    return addCount.reduce((total, item) => total + item);
+};
+// version 2
+// function countVowelConsonant(str) {
+//     const splitted = str.split('');
+//     const result = splitted.map(map_me);
+//     return result.reduce((total, item) => total + item);
+// };
+// function map_me(letter) {
+//     const vowels = ['a', 'e', 'i', 'o', 'u'];
+//     if (vowels.includes(letter)) {
+//         return 1;
+//     }
+//     return 2;
+// };
+// version 3
+// function countVowelConsonant(str) {
+//     const vowels = ['a', 'e', 'i', 'o', 'u'];
+//     let countOfVowelAndConsonant = 0;
+//     const splitted = str.split('');
+//     for (let i = 0; i < splitted.length; i++) {
+//         if (vowels.includes(splitted[i])) {
+//             countOfVowelAndConsonant++;
+//         } else {
+//             countOfVowelAndConsonant += 2;
+//         }
+//     }
+//     return countOfVowelAndConsonant;
+// };
+module.exports = countVowelConsonant;
+console.log(countVowelConsonant('hello'));
